50b3ca77adcbd921a1b68a3191a9632a1974c969
Loi <loivp13@gmail.com>
2021-09-20 13:20:41 -0700

    jagged array

---
+++ JaggedArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningVisualStudioC
+{
+    class JaggedArray
+    {
+        static void JaggedArray1(string[] args)
+        {
+            //declare jaggedArray
+            int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[5];
+            jaggedArray[0] = new int[3];
+            jaggedArray[0] = new int[2];
+
+            jaggedArray[0] = new int[] { 2, 3, 5, 7, 11 };
+            jaggedArray[1] = new int[] { 1,2,3 };
+            jaggedArray[0] = new int[] {13,21 };
+
+            //alternative way
+            int[][] jaggedArray2 = new int[][] { 
+                new int[] {2,3,5,7,11},
+                new int[] { 1,2,3}
+            };
+            
+        }
+    }
+}
