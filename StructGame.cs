442b9002bcb5f837a0e1273985d425ad13e7e133
Loi <loivp13@gmail.com>
2021-09-22 15:26:39 -0700

    struct

---
+++ StructGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningVisualStudioC
+{
+    struct StructGame
+    {
+        //similar to class
+        //CAN USE INTERFACE
+        //NO CONSTRUCTOR
+        //NO INHERITENCE
+
+        public string name;
+        public string developer;
+        public string rating;
+        public string releaseDate;
+
+        //methods
+        public void Display()
+        {
+            //runs code
+        }
+
+
+    }
+}
